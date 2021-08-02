b89d5e9db5d698dc0b59ed01b56be2d67a119ca1
Kyle Robison <kylerobison15@gmail.com>
2021-08-02 09:47:08 -0600

    build WAR, and deploy to EC2

--- OrderTracker/src/main/java/com/skilldistillery/orders/OrderTrackerApplication.java
+++ OrderTracker/src/main/java/com/skilldistillery/orders/OrderTrackerApplication.java
@@ -2,9 +2,16 @@ package com.skilldistillery.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OrderTrackerApplication {
+public class OrderTrackerApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OrderTrackerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderTrackerApplication.class, args);
