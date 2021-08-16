57f6a94f29516cc7c7858b612883bbcbd7be8237
Kyle Robison <kylerobison15@gmail.com>
2021-08-16 09:02:18 -0600

    add product updates

--- ngOrderTracker/src/app/app-routing.module.ts
+++ ngOrderTracker/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CustomerListComponent } from './components/customer-list/customer-list.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { OrderListComponent } from './components/order-list/order-list.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 const routes: Routes = [
 
   { path: '', pathMatch: 'full', redirectTo: 'order' },
   { path: 'order', component: OrderListComponent},
   { path: 'customer', component: CustomerListComponent},
+  { path: 'product', component: ProductListComponent},
   { path: '**', component: NotFoundComponent } // '**' is a wild card catch all path. We use this for our 404 page
 
 ];
